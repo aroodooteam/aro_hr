de20b9ea3b1076234d8765ac32fcd530894a955c
hraharison <hraharison@aro.mg>
2017-08-22 09:06:04 +0300

    correct hr applicant aptitude on odoo 7 to odoo 8

--- models/hr_applicant_aptitude.py
+++ models/hr_applicant_aptitude.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from openerp.osv import fields, osv
-import datetime
+from openerp import api, exceptions, fields, models, _
 
+#    _columns = {
+#        'name':fields.many2one('aptitude.type', 'Aptitude'),
+#        'applicant_id':fields.many2one('hr.applicant', 'Candidat'),
+#        'taux':fields.integer(u'Taux employé %'),
+#        }
+#hr_applicant_aptitude()
+class hr_applicant_aptitude(models.Model):
+	_name = 'hr.applicant.aptitude'
+    _description = 'Aptitudes d'un employe'
 
-class hr_applicant_aptitude(osv.osv):
-
-    _name = "hr.applicant.aptitude"
-    _description = "Aptitudes d'un employe"
-
-    _columns = {
-        'name':fields.many2one('aptitude.type', 'Aptitude'),
-        'applicant_id':fields.many2one('hr.applicant', 'Candidat'),
-        'taux':fields.integer(u'Taux employé %'),
-        }
-hr_applicant_aptitude()
+    #name = fields.Char(string='Aptitude', size=64)
+    _name = fields.many2one('aptitude.type', 'Aptitude')
+    _applicant_id=fields.many2one('hr.applicant','Candidat')
+    _taux=fields.integer(u'Taux amployé %')
