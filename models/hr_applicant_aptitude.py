924f68b4d5c6270f19e3e33ae9eeb0438d651449
hraharison <hraharison@aro.mg>
2017-08-22 10:39:13 +0300

    correction hr_applicant_aptitude finale

--- models/hr_applicant_aptitude.py
+++ models/hr_applicant_aptitude.py
@@ -4,9 +4,9 @@ from openerp import api, exceptions, fields, models, _
 
 
 class HrApplicantAptitude(models.Model):
-	_name = 'hr.applicant.aptitude'
- 	_description = 'Aptitudes d'un employe'
+    _name = 'hr.applicant.aptitude'
+    _description = '''Aptitudes d'un employe'''
 
-	name = fields.many2one('aptitude.type', 'Aptitude')
-	applicant_id=fields.many2one('hr.applicant','Candidat')
- 	taux=fields.integer(u'Taux amployé %')
+    name = fields.Many2one(string='Aptitude', comodel_name='aptitude.type')
+    applicant_id = fields.Many2one(string='Candidat', comodel_name='hr.applicant')
+    taux = field_name = fields.Integer(string= u'Taux employé %')
