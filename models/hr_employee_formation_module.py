cb50095acfc362d61a9f7d63fdad4c516696f7e6
hraharison <hraharison@aro.mg>
2017-08-24 18:10:12 +0300

    formation module

--- models/hr_employee_formation_module.py
+++ models/hr_employee_formation_module.py
@@ -1,18 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from openerp.osv import fields, osv
-import datetime
+from openerp import api, exceptions, fields, models, _
 
-
-class hr_employee_formation_module(osv.osv):
+class HrEmployeeFormationModule(models.Model):
     """Gestion des modules de formation chez ARO"""
-    _name = 'hr.employee.formation.module'
-    _columns = {
-        'code':fields.char('Code', size=16),
-        'name':fields.char('Libellé', size=64),
 
-        # 'cout':fields.float('Coût'), ##champs en stand by
-        # 'branche_id':fields.char('Branche',size=16), ##champs en stand by
-        # 'specialite':fields.char('Specialite',size=32), ##champs en stand by
-    }
-hr_employee_formation_module()
+    _name = 'hr.employee.formation.module'
+
+ 	code = fields.Char(string='Code',size=16)
+ 	name = fields.Char(string='Libellé',size=64)
