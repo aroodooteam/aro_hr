f66d5d190cfc63227fd258ad5c1b174f93f05f5f
hraharison <hraharison@aro.mg>
2017-08-29 09:17:58 +0300

    manala %

--- models/hr_employee_last_job.py
+++ models/hr_employee_last_job.py
@@ -8,7 +8,7 @@ class HrEmployeeLastJob(models.Model):
     _name = 'hr.employee.last.job'
 
     employee_id = fields.Many2one(string='Employe',comodel_name='hr.employee')
-    annee = fields.Char(string=u'Année%',size=16)
+    annee = fields.Char(string=u'Année',size=16)
     poste = fields.Char(string='Poste',size=32)
     employeur = fields.Char(string='Employeur',size=32)
     ref = fields.Char(string='Reference',size=32)
