e6b04d5800e5eca3e12801ff3f6fbb06279aac86
aroodoo_asus_hary <aroodoo@asus.aro>
2017-08-30 11:08:07 +0300

    Fix attributes of class

--- models/hr_job_aptitude.py
+++ models/hr_job_aptitude.py
@@ -3,7 +3,7 @@
 from openerp import api, exceptions, fields, models, _
 
 
-class HrJobAptitude(models.model):
+class HrJobAptitude(models.Model):
 
     _name = "hr.job.aptitude"
     _description = "Aptitude necessaire pour un poste"
