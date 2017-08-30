b96ef8e4f735c8c41915d7ef393a525bf2183d74
aroodoo_asus_hary <aroodoo@asus.aro>
2017-08-30 15:18:29 +0300

    Remove comma in end of field

--- models/hr_employee_note.py
+++ models/hr_employee_note.py
@@ -8,8 +8,8 @@ class HrEmployeeNote(models.Model):
     _name = 'hr.employee.note'
     _description = "Employee Note"
 
-    employee_id = fields.Many2one( comodel_name = 'hr.employee', string = 'Employe'),
-    annee = fields.Char( string = 'Annee', size=16),
-    note = fields.Selection((('a', 'A'), ('b', 'B'), ('b+', 'B+'), ('c', 'C'), ('c+', 'C+'), ('d', 'D')), string = 'Note'),
-    mois = fields.Char(string = 'Mois', size=32),
-    ref = fields.Char( string = 'Reference', size=32),
+    employee_id = fields.Many2one(comodel_name='hr.employee', string='Employe')
+    annee = fields.Char( string = 'Annee', size=16)
+    note = fields.Selection((('a', 'A'), ('b', 'B'), ('b+', 'B+'), ('c', 'C'), ('c+', 'C+'), ('d', 'D')), string='Note')
+    mois = fields.Char(string='Mois', size=32)
+    ref = fields.Char(string='Reference', size=32)
