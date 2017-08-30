d869670db1a3125944ed632e7b0531f75459dff2
aroodoo_asus_hary <aroodoo@asus.aro>
2017-08-30 11:02:03 +0300

    Fix tab in employee_note

--- models/hr_employee_note.py
+++ models/hr_employee_note.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from openerp import api, exceptions, fields, models,
+from openerp import api, exceptions, fields, models
 
 
 class HrEmployeeNote(models.Model):
     """Gestion des notes des employes"""
     _name = 'hr.employee.note'
     _description = "Employee Note"
-    
-        employee_id = fields.Many2one( comodel_name = 'hr.employee', string = 'Employe'),
-        annee = fields.Char( string = 'Annee', size=16),
-        note = fields.Selection((('a', 'A'), ('b', 'B'), ('b+', 'B+'), ('c', 'C'), ('c+', 'C+'), ('d', 'D')), string = 'Note'),
-        mois = fields.Char(string = 'Mois', size=32),
-        ref = fields.Char( string = 'Reference', size=32),
-    
 
-
+    employee_id = fields.Many2one( comodel_name = 'hr.employee', string = 'Employe'),
+    annee = fields.Char( string = 'Annee', size=16),
+    note = fields.Selection((('a', 'A'), ('b', 'B'), ('b+', 'B+'), ('c', 'C'), ('c+', 'C+'), ('d', 'D')), string = 'Note'),
+    mois = fields.Char(string = 'Mois', size=32),
+    ref = fields.Char( string = 'Reference', size=32),
