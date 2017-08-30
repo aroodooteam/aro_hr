b01650c532534f1a12e453fc418f4756ce6995d4
aroodoo_asus_hary <aroodoo@asus.aro>
2017-08-30 19:08:28 +0300

    Fix indentation

--- models/medical_type.py
+++ models/medical_type.py
@@ -4,8 +4,8 @@ from openerp import api, exceptions, fields, models
 
 
 class MedicalType(models.Model):
-        _name = 'medical.type'
-        _description = "Medical Type"
+    _name = 'medical.type'
+    _description = "Medical Type"
 
-        name = fields.Char(string = 'Description', size=32)
-        detail = fields.Text(string = 'Text sur document')
+    name = fields.Char(string = 'Description', size=32)
+    detail = fields.Text(string = 'Text sur document')
