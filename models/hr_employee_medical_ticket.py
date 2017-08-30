8ee672926259ee9bf02fd19bc016783f802ceef9
aroodoo_asus_hary <aroodoo@asus.aro>
2017-08-30 11:06:41 +0300

    Fix tab

--- models/hr_employee_medical_ticket.py
+++ models/hr_employee_medical_ticket.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from openerp import api, exceptions, fields, models,
+from openerp import api, exceptions, fields, models
 
 
 class HrEmployeeMedicalTicket(models.Model):
@@ -8,14 +8,9 @@ class HrEmployeeMedicalTicket(models.Model):
     _name = 'hr.employee.medical.ticket'
     _description = "Employee Medical Ticket"
 
-    
-        date = fields.Datetime(string = 'Date')
-        name = fields.Many2one(comodel_name = 'medical.type', string = 'Medical')
-        repos = fields.Boolean(string = 'Avec repos')
-        date_debut_repos = fields.Date( string = 'Date debut repos')
-        date_fin_repos = fields.Date(string = 'Date reprise de poste')
-        employee_id = fields.Many2one(comodel_name ='hr.employee', string = 'Salarie')
-    
-
-
-
+    date = fields.Datetime(string='Date')
+    name = fields.Many2one(comodel_name='medical.type', string='Medical')
+    repos = fields.Boolean(string = 'Avec repos')
+    date_debut_repos = fields.Date(string='Date debut repos')
+    date_fin_repos = fields.Date(string='Date reprise de poste')
+    employee_id = fields.Many2one(comodel_name='hr.employee', string='Salarie')
