ee8ea0791e89793f8141a7e0ba0a109f5908845f
aroodoo_asus_hary <aroodoo@asus.aro>
2017-08-30 11:01:00 +0300

    Fix tab

--- models/medical_type.py
+++ models/medical_type.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from openerp import api, exceptions, fields, models,
+from openerp import api, exceptions, fields, models
 
 
 class MedicalType(models.Model):
         _name = 'medical.type'
         _description = "Medical Type"
 
-        
-            name = fields.Char(string = 'Description', size=32)
-            detail = fields.Text(string = 'Text sur document')
-        
-
+        name = fields.Char(string = 'Description', size=32)
+        detail = fields.Text(string = 'Text sur document')
