a65d3e65ed35241a9d4eaaee36474a96ae1fc9b6
hraharison <hraharison@aro.mg>
2017-08-31 17:34:28 +0300

    migration applicant to odoo 8

--- models/hr_applicant.py
+++ models/hr_applicant.py
@@ -1,17 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from openerp.osv import fields, osv
-import datetime
+from openerp import api, exceptions, fields, models, _
 
-class hr_applicant(osv.osv):
+class HrApplicant(models.Model):
     _inherit = 'hr.applicant'
 
-    _columns = {
-        'qualification_ids':fields.one2many('hr.applicant.qualification','applicant_id','Qualifications'),
-        'formation_ids':fields.one2many('hr.applicant.formation','applicant_id','Formation'),#add by Hari
-        'aptitude_ids':fields.one2many('hr.applicant.aptitude','applicant_id','Aptitudes'),
-        'job_qualification_ids':fields.related('job_id','qualification_ids',string='Qualifications Requise',type='one2many',relation='hr.job.qualification'),
-        'job_aptitude_ids':fields.related('job_id','aptitude_ids',string='Aptitudes Requise',type='one2many',relation='hr.job.aptitude' ),
-        'job_qualification_ids':fields.related('job_id','qualification_ids',string='Qualifications Requise',type='one2many',relation='hr.job.qualification'),
-        'job_formation_ids':fields.related('job_id','formation_ids',string='Formations necessaire',type='one2many',relation='hr.job.formation'),
-    }
+    qualification_ids = fields.One2many(string='Qualifications',comodel_name='hr.applicant.qualification',inverse_name='applicant_id')
+    formation_ids = fields.One2many(string='Formation',comodel_name='hr.applicant.formation',inverse_name='applicant_id')
+    aptitude_ids = fields.One2many(string='Aptitudes',comodel_name='hr.applicant.aptitude',inverse_name='applicant_id')
+    job_qualification_ids = fields.One2many(string='Qualifications Requise',related='job_id.qualification_ids',comodel_name='hr.job.qualification')
+    job_aptitude_ids = fields.One2many(string='Qualifications Requise',related='job_id.aptitude_ids',comodel_name='hr.job.aptitude')
+    job_formation_ids = fields.One2many(string='Formations necessaire',related='job_id.formation_ids',comodel_name='hr.job.formation')
