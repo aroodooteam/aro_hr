66a30aaf0fa9ee3b1da0964fc7a0bfb1e9d14072
tovoheryRabako <trabako@aro.mg>
2017-09-05 17:54:01 +0300

    modif qual_type

--- models/qual_type.py
+++ models/qual_type.py
@@ -6,4 +6,4 @@ class QualType(models.Model):
     _name = 'qual.type'
     
     name = fields.Char( string = 'Description')
-    code = :fields.Char(string = u'Code diplôme', size=16)
+    code = fields.Char(string = u'Code diplôme', size=16)
