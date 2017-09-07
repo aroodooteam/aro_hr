ce72aca796f3ee8a4aea9624701484c387d9afb3
hraharison <hraharison@aro.mg>
2017-09-07 15:47:09 +0300

    migration function visibility odoo7 into odoo8

--- models/hr_employee.py
+++ models/hr_employee.py
@@ -10,6 +10,7 @@ logger = logging.getLogger(__name__)
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
+
     # def _get_chargefam(self, cr, uid, ids, field_name, arg, context):
     #     employees = self.browse(cr, uid, ids)
     #     res = {}
@@ -23,32 +24,70 @@ class HrEmployee(models.Model):
     #         res[employee.id] = count
     #     return res
 
+
+
+
     @api.multi
     def _get_chargefam(self):
         for emp in self:
             ages_list = emp.mapped('children_ids.age')
             emp.chargefam = sum(int(age.split('a')[0]) < 21 for age in ages_list)
 
-    def _get_visibility(self, cr, uid, ids, field_name=None, args=None, context=None):
-        res = {}
-        for emp in self.browse(cr, uid, ids, context=context):
+    # uid = self._uid
+    #def _get_visibility(self, cr, uid, ids, field_name=None, args=None, context=None):
+    #   res = {}
+    #   for emp in self.browse(cr, uid, ids, context=context):
+    #       visible = False
+    #       if emp.user_id.id == uid:
+    #           visible = True
+    #       elif emp.parent_id.user_id.id == uid:
+    #           visible = True
+    #       else:
+    #           group_ids = self.pool.get('res.users').browse(
+    #               cr, uid, uid, context=context).groups_id
+    #           group_user_id = self.pool.get("ir.model.data").get_object_reference(cr, uid, 'base', 'group_hr_user')[1]
+    #           if group_user_id in [group.id for group in group_ids]:
+    #               visible = True
+    #           else:
+    #               group_user_id = self.pool.get("ir.model.data").get_object_reference(cr, uid, 'base', 'group_hr_manager')[1]
+    #               if group_user_id in [group.id for group in group_ids]:
+    #                   visible = True
+    #       res[emp.id] = visible
+    #   return res
+
+    @api.multi
+    def _get_visibility2(self):
+        user_obj = self.env['res.users']
+        for emp in self:
+            uid = self._uid
             visible = False
-            if emp.user_id.id == uid:
-                visible = True
-            elif emp.parent_id.user_id.id == uid:
+            if emp.user_id.id == uid or emp.parent_id.user_id.id == uid:
                 visible = True
             else:
-                group_ids = self.pool.get('res.users').browse(
-                    cr, uid, uid, context=context).groups_id
-                group_user_id = self.pool.get("ir.model.data").get_object_reference(cr, uid, 'base', 'group_hr_user')[1]
+                group_ids = user_obj.browse(uid).groups_id
+                group_user_id = self.env.ref('base.group_hr_user')
                 if group_user_id in [group.id for group in group_ids]:
                     visible = True
                 else:
-                    group_user_id = self.pool.get("ir.model.data").get_object_reference(cr, uid, 'base', 'group_hr_manager')[1]
+                    group_user_id = self.env.ref('base.group_hr_manager')
                     if group_user_id in [group.id for group in group_ids]:
                         visible = True
-            res[emp.id] = visible
-        return res
+            emp.visible = visible
+
+
+    @api.multi
+    def _get_visibility(self):
+        user_obj = self.env['res.users']
+        for emp in self:
+            uid = self._uid
+            visible = False
+            group_ids = user_obj.browse(uid).groups_id
+            group_user_id_user = self.env.ref('base.group_hr_user')
+            group_user_id_manager = self.env.ref('base.group_hr_manager')
+            if (group_user_id_user in [group.id for group in group_ids]) or (group_user_id_manager in [group.id for group in group_ids]) \
+            or (emp.user_id.id == uid) or (emp.parent_id.user_id.id == uid):
+                visible = True
+            emp.visible = visible
 
     def _wb(self, cr, uid, ids, field_name=None, arg=None, context=None):
         employees = self.read(cr, uid, ids, ['birthday', 'id'], context)
@@ -68,7 +107,7 @@ class HrEmployee(models.Model):
     cin = fields.Char(string='CIN', size=64)
     cin_date = fields.Date(string='Date CIN')
     cin_place = fields.Char(string='Lieu CIN', size=30)
-    chargefam = fields.Float(string='xxxxxxx???', compute='_get_chargefam')
+    chargefam = fields.Float(string='Charge Familliale', compute='_get_chargefam')
     visible = fields.Boolean(string='Visible',compute='_get_visibility')
     payment_term_id = fields.One2many(string='Mode de Paiement',comodel_name='payment.term',inverse_name='employee_id')
     anciennete = fields.Boolean(string='Prime anciennete',help=u'Est ce que cet employe benificie de la prime d\'anciennete')
