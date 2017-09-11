ff13969b36aa16868c3f780ea5cb69855666cff4
aroodoo_asus_hary <aroodoo@asus.aro>
2017-09-11 15:56:43 +0300

    [CLN] clean task categ

--- models/hr_task_categ.py
+++ models/hr_task_categ.py
@@ -2,8 +2,9 @@
 
 from openerp import api, exceptions, fields, models, _
 
+
 class HrTaskCateg(models.Model):
     _name = 'hr.task.categ'
     _description = "Category"
 
-    name = fields.Char (string = 'Category')
+    name = fields.Char (string='Category')
